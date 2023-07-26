f6ab79e5a9aa5d74054bf6f39f36d319b10f8bf0
Valeriya Venediktova <leravened2001@gmail.com>
2023-07-26 15:17:07 +0300

    hw3

---
+++ hw3/hw3.py
@@ -0,0 +1,51 @@
+import csv
+import json
+
+with open('books.csv', 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    books = [line for line in csv_reader]
+
+
+with open('users.json', "r") as json_file:
+    users = json.load(json_file)
+
+quantity_books = len(books)
+quantity_users = len(users)
+
+books_to_each = int(quantity_books / quantity_users)
+books_left = quantity_books % quantity_users
+
+result_list = []
+
+book_index = 0
+for user in users:
+    user_info = {
+        "name": user["name"],
+        "gender": user["gender"],
+        "address": user["address"],
+        "age": user["age"],
+        "books": []
+    }
+    for _ in range(books_to_each):
+        user_info["books"].append({
+            "Title": books[book_index]["Title"],
+            "Author": books[book_index]["Author"],
+            "Pages": books[book_index]["Pages"],
+            "Genre": books[book_index]["Genre"]
+        })
+        book_index =+ 1
+    if books_left > 0:
+        user_info["books"].append({
+            "Title": books[book_index]["Title"],
+            "Author": books[book_index]["Author"],
+            "Pages": books[book_index]["Pages"],
+            "Genre": books[book_index]["Genre"]
+        })
+        book_index += 1
+        books_left -= 1
+
+    result_list.append(user_info)
+
+
+with open("result.json", "w") as json_file:
+    json.dump(result_list, json_file, indent=4)
